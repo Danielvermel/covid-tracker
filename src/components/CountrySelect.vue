4877a1e06f490bea8be76862d39519fee2891395
Daniel Leite <daniel.leite@nexuspoint.co.uk>
2022-01-02 10:00:59 +0000

    add data and main components

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,35 @@
+<template>
+    <select 
+        @change="onChange"
+        v-model="selected" 
+        class="form-select block w-full p-3 rounded mt-10"
+    >
+        <option value="0">Select Country</option>
+        <option 
+            v-for="country,index in countries"
+            :key="index" 
+            :value="country.ID"
+        >
+            {{country.Country}}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+            this.$emit('get-country', country)
+        }
+    }
+}
+</script>
